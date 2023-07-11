badead8745d9d8e3a63d56a02d0b5923f98713b9
dosdedoce <dosdedoce@gmail.com>
2023-07-11 10:43:42 +0200

    Version final

--- src/saxon/Saxon.js
+++ src/saxon/Saxon.js
@@ -1,3 +1,22 @@
-class Saxon {}
+import Soldier from '../soldier/Soldier.js';
+const Soldier=require('../soldier/Soldier.js');
+
+class Saxon extends Soldier {
+    constructor(health , strength) {
+        super(health , strength);
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health-=damage;
+        if(this.health<=0){
+            return `A Saxon has died in act of combat`
+        } 
+        if(this.health>0){
+            return `A Saxon has received ${damage} points of damage`
+        }
+    }
+}
 
 module.exports = Saxon;
